8f6192c121bbef40eb85f49f7a6a87f3268d0a48
Varun <vkvarun26@gmail.com>
2023-05-17 18:08:13 +1000

    POST method of registration updated - v1.0

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const app = express();
-
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("user.db");
 const bodyParser = require("body-parser");
 
 app.use(express.static(__dirname + "/public"));
 app.engine("html", require("ejs").renderFile);
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -37,6 +35,7 @@ app.post("/login", (req, res) => {
       res.status(401).send("Invalid password");
     } else {
       res.status(200).send("Login successful");
+      db.close();
     }
   });
 });
@@ -44,19 +43,63 @@ app.post("/login", (req, res) => {
 //registration
 app.post("/register", (req, res) => {
   const { firstname, lastname, studentId, email, password } = req.body;
-  console.log(req.body.firstname);
-  db.run(
-    "INSERT INTO users (firstname, lastname, studentId, email, password) VALUES (?, ?, ?, ?, ?)",
-    [firstname, lastname, studentId, email, password],
-    function (err) {
-      if (err) {
-        console.error(err);
-        res.status(500).send("Internal Server Error");
+
+  // check if studentId already exists
+  findAllstudentId_query = "SELECT * FROM users WHERE studentId = ?";
+  db.all(findAllstudentId_query, [studentId], (err, rows) => {
+    if (err) {
+      console.error(err);
+    } else if (rows) {
+      let flag = true;
+      rows.forEach((row) => {
+        if (row.studentId === studentId) {
+          flag = false;
+        }
+      });
+
+      // studentId doesn't exists -> registered successfully
+      if (flag) {
+        insertData_query =
+          "INSERT INTO users (firstname, lastname, studentId, email, password) VALUES (?, ?, ?, ?, ?)";
+        db.run(
+          insertData_query,
+          [firstname, lastname, studentId, email, password],
+          function (err) {
+            if (err) {
+              console.error(err);
+              res.status(500).send("Internal Server Error");
+            } else {
+              //find userId through studentId and
+              //insert transcript and certificate file_names in db
+              getUserId_query = "SELECT userId FROM users WHERE studentId = ?";
+              db.get(getUserId_query, studentId, (err, { userId }) => {
+                transcript_name = userId + "_transcript";
+                certificate_name = userId + "_cert";
+                updateFilenames_query =
+                  "UPDATE users SET transcript = ?, certificate = ? WHERE studentId = ?";
+                db.run(
+                  updateFilenames_query,
+                  [transcript_name, certificate_name, studentId],
+                  (err, rows) => {
+                    console.log(`rows`, rows);
+                    db.close();
+                  }
+                );
+              });
+              res.status(200).send("Registration successful");
+            }
+          }
+        );
       } else {
-        res.status(200).send("Registration successful");
+        // studentId already exists -> show Error
+        res
+          .status(401)
+          .send(
+            "Student ID already registered. Register with new student ID or login with existing student ID."
+          );
       }
     }
-  );
+  });
 });
 
 app.listen(3000, () => {
